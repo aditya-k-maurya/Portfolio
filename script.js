a7d30858dceb37b4c4315c1f6501d3db83856cd4
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-04-08 00:40:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//  typing animation
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "Competitive Programmer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop:true
+})
+ 
+// =============== Aside ================================
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length;
+
+for (let i = 0; i < totalNavList; i++){
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    this.classList.add("active")
+  })
+}
