2e55fc3a55ccd67c91c9eb8d316d3b8aab83d873
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-07-12 18:15:14 +0530

    portfolio update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,25 @@ import { ActiveSectionProvider } from "@/context/ActiveSectionContext";
 export const metadata: Metadata = {
 	title: "Aditya | Portfolio",
 	description: "This is my personal profile website, I hope you like it :) ",
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon.ico",
+		apple: "/apple-touch-icon.png",
+		other: [
+			{
+				rel: "icon",
+				url: "/favicon-32x32.png",
+				sizes: "32x32",
+				type: "image/png",
+			},
+			{
+				rel: "icon",
+				url: "/favicon-16x16.png",
+				sizes: "16x16",
+				type: "image/png",
+			},
+		],
+	},
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -31,33 +50,11 @@ function Content({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<Head>
-				{/* Favicons */}
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
 
 				{/* Style Switcher */}
 				<link rel="stylesheet" href="@/components/css/style-switcher.css" />
 
-				{/* Fonts */}
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link rel="preload" href="https://fonts.gstatic.com/s/poppins/v21/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+
 
 			</Head>
 			<ModeProvider>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
 import PortfolioItem from "./PortfolioItem";
-import { portfolioData } from "../../public/data/portfolioData";
+import {
+	portfolioData,
+	portfolioFunData,
+} from "../../public/data/portfolioData";
 
 function Portfolio({ section, backSection }: any) {
 	return (
@@ -39,7 +42,7 @@ function Portfolio({ section, backSection }: any) {
 					))}
 				</div>
 
-				<div className="row">
+				<div className="row mt-8">
 					<div className="portfolio-heading padd-15">
 						<h2>Some Fun Projects:</h2>
 					</div>
@@ -47,112 +50,11 @@ function Portfolio({ section, backSection }: any) {
 
 				<div className="row">
 					{/* <!-- ===== Portfolio item ====== --> */}
-					<div className="portfolio-item padd-15">
-						<div className="portfolio-item-inner shadow-dark">
-							<a
-								href="https://aditya-k-maurya.vercel.app/#home"
-								className="portfolio-img">
-								<img src="assets/portfolio-img.png" alt="" />
-							</a>
-							<div className="portfolio-item-content">
-								<h2 className="portfolio-item-heading">Hangman</h2>
-								<ul className="portfolio-item-tags">
-									<li className="portfolio-tag-main">TypeScript</li>
-									<li>Reactjs</li>
-									<li>tailwind</li>
-									<li>css</li>
-								</ul>
-
-								<div className="portfolio-item-description">
-									Hangman a word guess game before the man hangs.
-								</div>
-
-								<a className="portfolio-item-btn">Repositery</a>
-							</div>
+					{portfolioFunData.map((data) => (
+						<div className="portfolio-item padd-15">
+							<PortfolioItem data={data} key={data.id} />
 						</div>
-					</div>
-
-					<div className="portfolio-item padd-15">
-						<div className="portfolio-item-inner shadow-dark">
-							<a
-								href="https://aditya-k-maurya.github.io/tour-booking"
-								target="_blank"
-								className="portfolio-img">
-								<img src="assets/tourism.png" alt="" />
-							</a>
-							<div className="portfolio-item-content">
-								<h2 className="portfolio-item-heading">Drumkit</h2>
-								<ul className="portfolio-item-tags">
-									<li className="portfolio-tag-main">Javascript</li>
-									<li>html</li>
-									<li>css</li>
-								</ul>
-
-								<div className="portfolio-item-description">
-									Play musics to and create music as you want.
-								</div>
-
-								<a className="portfolio-item-btn">Repositery</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="portfolio-item padd-15">
-						<div className="portfolio-item-inner shadow-dark">
-							<a
-								href="https://aditya-react-projects.vercel.app/"
-								target="_blank"
-								className="portfolio-img">
-								<img src="assets/reactproj.png" alt="" />
-							</a>
-							<div className="portfolio-item-content">
-								<h2 className="portfolio-item-heading">React prjects</h2>
-								<ul className="portfolio-item-tags">
-									<li className="portfolio-tag-main">reactjs</li>
-									<li>javascript</li>
-									<li>CSS</li>
-									<li>redux</li>
-									<li>api-handling</li>
-									<li>hooks</li>
-									<li>tailwind</li>
-								</ul>
-
-								<div className="portfolio-item-description">
-									It contain multipe mini projects which I created to while
-									learning react or whenever I feel like to create something.
-								</div>
-
-								<a className="portfolio-item-btn">Repositery</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="portfolio-item padd-15">
-						<div className="portfolio-item-inner shadow-dark">
-							<a
-								href="https://aditya-k-maurya.github.io/Techsnap-main/timeline"
-								target="_blank"
-								className="portfolio-img">
-								<img src="assets/techsnap.png" alt="" />
-							</a>
-							<div className="portfolio-item-content">
-								<h2 className="portfolio-item-heading">Techsnap frontend</h2>
-								<ul className="portfolio-item-tags">
-									<li className="portfolio-tag-main">frontend</li>
-									<li>html</li>
-									<li>CSS</li>
-									<li>javascript</li>
-								</ul>
-
-								<div className="portfolio-item-description">
-									Fully responsive Techsnap new website frontend. Made during my
-									internship at Techsnap.
-								</div>
-
-								<a className="portfolio-item-btn">Repositery</a>
-							</div>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</section>
